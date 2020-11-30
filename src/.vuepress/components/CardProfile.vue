f7cc232948eb3c470ab0a5ec0bd85530d8c02eec
Amatsuti <oniprimsrq@yahoo.co.jp>
2020-11-30 09:35:01 +0900

    カード説明を一覧形式に

--- src/.vuepress/components/CardProfile.vue
+++ src/.vuepress/components/CardProfile.vue
@@ -1,33 +1,36 @@
 <template>
   <tr class="profile">
     <td class="name">{{ cardName }}</td>
-    <td class="ai">
+    <td class="select">
+      <div>条件</div>
       <ul>
         <li v-for="v in ai">
           {{ v.value }}
         </li>
       </ul>
-    </td>
-    <td class="input">
+      <div>入力</div>
       <ul>
         <li v-for="v in input">
           {{ v.value }}
         </li>
       </ul>
     </td>
-    <td class="start-action">
-      <ul>
-        <li v-for="v in startAction">
-          {{ v.value }}
-        </li>
-      </ul>
-    </td>
+    <td class="armor">{{ armor }}</td>
+    <td class="aria">{{ aria }}</td>
     <td class="action">
       <ul>
         <li v-for="v in action">
           {{ v.value }}
         </li>
       </ul>
+      <!-- <template v-if="etc.length > 0">
+        <div>その他</div>
+        <ul>
+          <li v-for="v in input">
+            {{ v.value }}
+          </li>
+        </ul>
+      </template> -->
     </td>
   </tr>
 </template>
@@ -46,11 +49,23 @@ export default {
     ai () { return this.innerValue["ai"] },
     input () { return this.innerValue["input"] },
     startAction () { return this.innerValue["start-action"] },
+    armor () { return this.innerValue.armor },
+    aria () { return this.innerValue.aria },
     action () { return this.innerValue["action"] },
+    etc () { return this.innerValue["etc"] },
   },
   data () {
     return {
-      innerValue: {}
+      innerValue: {
+        name: "",
+        ai: [],
+        input: [],
+        "start-action":[],
+        armor: 0,
+        aria: 0,
+        action: [],
+        etc: []
+      }
     }
   },
   created () {
@@ -81,6 +96,9 @@ export default {
     font-weight: bold;
     margin: 0.5rem 0;
   }
+  .name { width: 150px; }
+  .select, .action { width: 230px; vertical-align: top; }
+  .armor, .aria{ width: 15px; vertical-align: top; }
   ul {
     margin: 0;
     line-height: 1.0;
--- src/card/skill.md
+++ src/card/skill.md
@@ -6,6 +6,7 @@ sidebar: false
 # 攻撃系
 
 <table>
+  <tr><th>カード名</th><th>選択</th><th>耐久</th><th>速度</th><th>行動</th></tr>
   <CardProfile command="test-cmd/smash" />
   <CardProfile command="test-cmd/fire" />
 </table>
@@ -13,6 +14,7 @@ sidebar: false
 # 防御系
 
 <table>
+  <tr><th>カード名</th><th>選択</th><th>耐久</th><th>速度</th><th>行動</th></tr>
   <CardProfile command="test-cmd/guard" />
   <CardProfile command="test-cmd/protect" />
   <CardProfile command="test-cmd/avoid" />
