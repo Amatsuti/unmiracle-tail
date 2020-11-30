1966a16d2f31c6f01f19d661b1f58a323f035333
Amatsuti <oniprimsrq@yahoo.co.jp>
2020-11-30 11:01:42 +0900

    表にすると見せかけてやめる

--- src/.vuepress/components/CardProfile.vue
+++ src/.vuepress/components/CardProfile.vue
@@ -1,38 +1,52 @@
 <template>
-  <tr class="profile">
-    <td class="name">{{ cardName }}</td>
-    <td class="select">
-      <div>条件</div>
-      <ul>
-        <li v-for="v in ai">
-          {{ v.value }}
-        </li>
-      </ul>
-      <div>入力</div>
-      <ul>
-        <li v-for="v in input">
-          {{ v.value }}
-        </li>
-      </ul>
-    </td>
-    <td class="armor">{{ armor }}</td>
-    <td class="aria">{{ aria }}</td>
-    <td class="action">
-      <ul>
-        <li v-for="v in action">
-          {{ v.value }}
-        </li>
-      </ul>
-      <!-- <template v-if="etc.length > 0">
-        <div>その他</div>
-        <ul>
-          <li v-for="v in input">
-            {{ v.value }}
-          </li>
-        </ul>
-      </template> -->
-    </td>
-  </tr>
+  <div class="profile">
+    <div class="card"></div>
+    <div class="detail">
+      <div class="profile-first">
+        <div class="name">{{ cardName }}</div>
+
+      </div>
+      <div class="profile-second">
+        <div class="select">
+          <div>条件</div>
+          <ul>
+            <li v-for="v in ai">
+              {{ v.value }}
+            </li>
+          </ul>
+          <div>入力</div>
+          <ul>
+            <li v-for="v in input">
+              {{ v.value }}
+            </li>
+          </ul>
+        </div>
+        <!-- <div class="armor">{{ armor }}</div>
+        <div class="aria">{{ aria }}</div> -->
+        <div class="action">
+          <div class="start-action">
+            <div class="aria">速度：{{ aria }}</div>
+            <div class="armor">耐久：{{ armor }}</div>
+          </div>
+          <div class="action-detail">
+            <ul>
+              <li v-for="v in action">
+                {{ v.value }}
+              </li>
+            </ul>
+            <!-- <template v-if="etc.length > 0">
+              <div>その他</div>
+              <ul>
+                <li v-for="v in input">
+                  {{ v.value }}
+                </li>
+              </ul>
+            </template> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -88,17 +102,79 @@ export default {
 
 <style lang="scss" scoped>
 .profile {
+  position: relative;
+
+  margin: 3px 0;
+  width: 100%;
+
+  display: flex;
+
   ::v-deep p {
     margin: 0;
   }
 
-  .type {
-    font-weight: bold;
-    margin: 0.5rem 0;
+  .card, .name, .select, .action-detail, .armor, .aria {
+    border: solid 1px #000;
+    box-sizing: border-box;
   }
-  .name { width: 150px; }
-  .select, .action { width: 230px; vertical-align: top; }
-  .armor, .aria{ width: 15px; vertical-align: top; }
+  .card {
+    min-width: 80px;
+    min-height: 100px;
+    height: 100%;
+
+    border-right: 0px;
+  }
+  .profile-first {
+    width: calc(100%-80px);
+
+    display: flex;
+
+    .name {
+      width: 660px;
+      height:1.5rem;
+
+      border-bottom: 0px;
+
+      padding: 0 5px;
+
+      font-weight: bold;
+    }
+  }
+  .profile-second {
+    width: calc(100%-80px);
+    height: calc(100% - 1.5rem);
+
+    display: flex;
+
+    .select, .action {
+      width: 330px;
+      vertical-align: top;
+    }
+
+    .select {
+      border-right: 0px;
+      padding: 10px;
+    }
+    .start-action {
+      height: 1.5rem;
+      display: flex;
+
+      .aria {
+        width: 165px;
+        border-right: 0px;
+        border-bottom: 0px;
+      }
+      .armor {
+        width: 165px;
+        border-bottom: 0px;
+      }
+    }
+    .action-detail {
+      height: calc(100% - 1.5rem);
+      padding: 10px;
+    }
+  }
+
   ul {
     margin: 0;
     line-height: 1.0;
--- src/card/skill.md
+++ src/card/skill.md
@@ -5,18 +5,12 @@ sidebar: false
 
 # 攻撃系
 
-<table>
-  <tr><th>カード名</th><th>選択</th><th>耐久</th><th>速度</th><th>行動</th></tr>
-  <CardProfile command="test-cmd/smash" />
-  <CardProfile command="test-cmd/fire" />
-</table>
+<CardProfile command="test-cmd/smash" />
+<CardProfile command="test-cmd/fire" />
 
 # 防御系
 
-<table>
-  <tr><th>カード名</th><th>選択</th><th>耐久</th><th>速度</th><th>行動</th></tr>
-  <CardProfile command="test-cmd/guard" />
-  <CardProfile command="test-cmd/protect" />
-  <CardProfile command="test-cmd/avoid" />
-  <CardProfile command="test-cmd/barrier" />
-</table>
+<CardProfile command="test-cmd/guard" />
+<CardProfile command="test-cmd/protect" />
+<CardProfile command="test-cmd/avoid" />
+<CardProfile command="test-cmd/barrier" />
