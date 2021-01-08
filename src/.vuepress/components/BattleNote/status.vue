5d1581abe65e07417a1ee1f09def4e3e4cb7382a
Amatsuti <oniprimsrq@yahoo.co.jp>
2021-01-09 02:23:43 +0900

    敵対値増加ログ

--- src/.vuepress/components/BattleNote/status.vue
+++ src/.vuepress/components/BattleNote/status.vue
@@ -18,17 +18,37 @@
         </template>
       </div>
     </template>
+    <template v-for="s in allCharacterId">
+      <div :key="s" v-if="options.status['hate.'+s]"
+        class="damage">
+        {{allCharacter | whoIdIs(s) | name }} への敵対値が
+        <template v-if="options.status['hate.'+s] > 0">
+          <b :class="damageClass">
+            {{ options.status['hate.'+s] }}
+          </b>
+          増加！
+        </template>
+        <template v-else>
+          <b :class="damageClass">
+            {{ options.status['hate.'+s] }}
+          </b>
+          減少！
+        </template>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
 import base from './base'
+import _ from 'lodash'
 const fixStatus = ['STR','INT','VIT','MND','DEX','AGI']
 export default {
   name:'heal',
   extends: base,
   computed: {
     fixStatus () { return fixStatus },
+    allCharacterId () { return _.map(this.allCharacter, e=>e.id) },
     damageClass () {
       return 'm-damage'
     }
